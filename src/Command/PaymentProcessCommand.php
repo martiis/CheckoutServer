d38ddc599ba469d2fc420dc2f0f5c8ff58c4aa70
Martynas Sudintas <martynas.sudintas@ongr.io>
2015-09-27 09:04:08 +0000

    licence headers

--- src/Command/PaymentProcessCommand.php
+++ src/Command/PaymentProcessCommand.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the CheckoutServer package.
+ *
+ * (c) Martynas Sudintas <martynas.sudintas@ongr.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Martiis\CheckoutServer\Command;
 
 use Symfony\Component\Console\Command\Command;
--- src/Command/StorageProcessCommand.php
+++ src/Command/StorageProcessCommand.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the ONGR package.
+ * This file is part of the CheckoutServer package.
  *
- * (c) NFQ Technologies UAB <info@nfq.com>
+ * (c) Martynas Sudintas <martynas.sudintas@ongr.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
